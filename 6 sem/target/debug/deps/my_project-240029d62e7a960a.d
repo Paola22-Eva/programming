892b86189d70fe872ee90f0d5ee8f1d704e2c745
Ева <115064739+Paola22-Eva@users.noreply.github.com>
2025-03-25 16:53:01 +0300

    Add files via upload

---
+++ 6 sem/target/debug/deps/my_project-240029d62e7a960a.d
@@ -0,0 +1,5 @@
+C:\Users\KME\Desktop\Новая папка (4)\rust\my_project\target\debug\deps\libmy_project-240029d62e7a960a.rmeta: src\main.rs
+
+C:\Users\KME\Desktop\Новая папка (4)\rust\my_project\target\debug\deps\my_project-240029d62e7a960a.d: src\main.rs
+
+src\main.rs:
