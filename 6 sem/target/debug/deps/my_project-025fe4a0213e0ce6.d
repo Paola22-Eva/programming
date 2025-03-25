227720a172945ec39273a8896c58fc128775a197
Ева <115064739+Paola22-Eva@users.noreply.github.com>
2025-03-25 16:54:32 +0300

    Delete 6 sem/target directory

--- 6 sem/target/debug/deps/my_project-025fe4a0213e0ce6.d
+++
@@ -1,5 +0,0 @@
-C:\Users\KME\Desktop\Новая папка (4)\rust\my_project\target\debug\deps\libmy_project-025fe4a0213e0ce6.rmeta: src\main.rs
-
-C:\Users\KME\Desktop\Новая папка (4)\rust\my_project\target\debug\deps\my_project-025fe4a0213e0ce6.d: src\main.rs
-
-src\main.rs:
--- 6 sem/target/debug/deps/my_project-240029d62e7a960a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\KME\Desktop\Новая папка (4)\rust\my_project\target\debug\deps\libmy_project-240029d62e7a960a.rmeta: src\main.rs
-
-C:\Users\KME\Desktop\Новая папка (4)\rust\my_project\target\debug\deps\my_project-240029d62e7a960a.d: src\main.rs
-
-src\main.rs:
